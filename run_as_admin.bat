6c0924fbe94cd3424b9ac99ace01a32a813eeaca
Zcc522527 <152373769+Zcc522527@users.noreply.github.com>
2025-11-29 16:43:20 +0800

    Create batch file to request admin privileges

---
+++ run_as_admin.bat
@@ -0,0 +1,24 @@
+@echo off
+:: 请求管理员权限
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+if '%errorlevel%' NEQ '0' (
+    echo 请求管理员权限...
+    goto UACPrompt
+) else (
+    goto gotAdmin
+)
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+:: 以管理员权限运行主程序
+call run_mouse_clicker.bat
